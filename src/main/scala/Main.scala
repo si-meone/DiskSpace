b454fd7d3cbb5153d5aef1ba01765e26b0399848
Nasrallah <simon_nasrallah@hotmail.com>
2013-10-13 23:05:37 +0100

    more intial commits

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,11 @@
+import java.io.File
+import java.nio.file.{Files, Path}
+
 object Main {
+  def getFolderSize(p: Path): Long = {
+    Files.size(p)
+  }
+
   def main(args: Array[String]) {
     println("Hello, World!")
     println("# of arguments: %d" format count(args))
--- src/test/scala/MainSuite.scala
+++ src/test/scala/MainSuite.scala
@@ -1,3 +1,5 @@
+import java.io.{FileOutputStream, File}
+import java.nio.file.Path
 import org.scalatest.FunSuite
 
 class MainSuite extends FunSuite {
@@ -11,4 +13,28 @@ class MainSuite extends FunSuite {
     assert(Main.count(Map(1 -> 1)) == 1)
     assert(Main.count(Set(1)) == 1)
   }
+
+  test("get file size") {
+    val folder = System.getProperty("java.io.tmpdir")
+    val file = new File(folder, "file1.txt")
+    fillFileToSize(file, 5)
+    println(file.getAbsolutePath)
+    assert(Main.getFolderSize(file.toPath) == 5)
+  }
+
+
+  def fillFileToSize(file: File ,size: Int) {
+    val stream = new FileOutputStream(file);
+
+    val buff = new Array[Byte](size)
+    stream.write(buff)
+    stream.flush()
+    stream.close()
+  }
+
+
+}
+
+class ScalaByte{
+  val bytes:Array[Byte] = "this is my test".getBytes()
 }

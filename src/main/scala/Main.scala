8e070339870353e82bfac6dd100610cd7bf7edb5
Simon Nasrallah <snasrallah@ebay.com>
2014-02-05 18:31:32 +0000

    initial work to send 'du' command and capture output

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,15 +1,64 @@
-import java.io.File
-import java.nio.file.{Files, Path}
+import sys.process._
+import collection.immutable.SortedMap
+
+class StorageUnit(val bytes: Long) extends Ordered[StorageUnit] {
+  def inBytes     = bytes
+  def inKilobytes = bytes / (1024L)
+  def inMegabytes = bytes / (1024L * 1024)
+  def inGigabytes = bytes / (1024L * 1024 * 1024)
+  def inTerabytes = bytes / (1024L * 1024 * 1024 * 1024)
+  def inPetabytes = bytes / (1024L * 1024 * 1024 * 1024 * 1024)
+  def inExabytes  = bytes / (1024L * 1024 * 1024 * 1024 * 1024 * 1024)
+
+  def +(that: StorageUnit): StorageUnit = new StorageUnit(this.bytes + that.bytes)
+  def -(that: StorageUnit): StorageUnit = new StorageUnit(this.bytes - that.bytes)
+  def *(scalar: Double): StorageUnit = new StorageUnit((this.bytes.toDouble*scalar).toLong)
+
+  override def equals(other: Any) = {
+    other match {
+      case other: StorageUnit =>
+        inBytes == other.inBytes
+      case _ =>
+        false
+    }
+  }
+
+  override def compare(other: StorageUnit) =
+    if (bytes < other.bytes) -1 else if (bytes > other.bytes) 1 else 0
+
+  override def toString() = inBytes + ".bytes"
+
+  def toHuman(): String = {
+    val prefix = "KMGTPE"
+    var prefixIndex = -1
+    var display = bytes.toDouble.abs
+    while (display > 1126.0) {
+      prefixIndex += 1
+      display /= 1024.0
+    }
+    if (prefixIndex < 0) {
+      "%d B".format(bytes)
+    } else {
+      "%.1f %cB".format(display*bytes.signum, prefix.charAt(prefixIndex))
+    }
+  }
+}
 
 object Main {
-  def getFolderSize(p: Path): Long = {
-    Files.size(p)
-  }
 
   def main(args: Array[String]) {
-    println("Hello, World!")
-    println("# of arguments: %d" format count(args))
-  }
+//    val path = args(0)
+    val path = "/"
 
-  def count[T](it: Iterable[T]): Int = it.size
+    println(Console.BLINK + s" scanning $path ")
+
+    val result = s"sudo du -b -d 1 $path"  #| "tr \"\\t\" \",\"\"" !!
+
+    val resultMap = result.split("\n").map(_ split ",").foldLeft(Map[Long, String]())((m,s) => m + (s(0).toLong -> s(1)))
+
+    val resultMapSorted =  resultMap.toSeq.sortBy(_._1).reverse
+
+    //for ((k,v) <- resultMapSorted) println(Console.YELLOW + v  + Console.WHITE + " -> " + Console.GREEN + new StorageUnit(k).toHuman + Console.RESET)
+    for ((k,v) <- resultMapSorted) println(s" $v  ->  " + new StorageUnit(k).toHuman)
+  }
 }
--- src/test/scala/MainSuite.scala
+++ src/test/scala/MainSuite.scala
@@ -1,40 +1,40 @@
-import java.io.{FileOutputStream, File}
-import java.nio.file.Path
-import org.scalatest.FunSuite
-
-class MainSuite extends FunSuite {
-  test("counting an empty collection") {
-    assert(Main.count(Map()) == 0)
-    assert(Main.count(Set()) == 0)
-  }
-
-  test("counting a non-empty collection") {
-    assert(Main.count(Array(1)) == 1)
-    assert(Main.count(Map(1 -> 1)) == 1)
-    assert(Main.count(Set(1)) == 1)
-  }
-
-  test("get file size") {
-    val folder = System.getProperty("java.io.tmpdir")
-    val file = new File(folder, "file1.txt")
-    fillFileToSize(file, 5)
-    println(file.getAbsolutePath)
-    assert(Main.getFolderSize(file.toPath) == 5)
-  }
-
-
-  def fillFileToSize(file: File ,size: Int) {
-    val stream = new FileOutputStream(file);
-
-    val buff = new Array[Byte](size)
-    stream.write(buff)
-    stream.flush()
-    stream.close()
-  }
-
-
-}
-
-class ScalaByte{
-  val bytes:Array[Byte] = "this is my test".getBytes()
-}
+//import java.io.{FileOutputStream, File}
+//import java.nio.file.Path
+//import org.scalatest.FunSuite
+//
+//class MainSuite extends FunSuite {
+//  test("counting an empty collection") {
+//    assert(Main.count(Map()) == 0)
+//    assert(Main.count(Set()) == 0)
+//  }
+//
+//  test("counting a non-empty collection") {
+//    assert(Main.count(Array(1)) == 1)
+//    assert(Main.count(Map(1 -> 1)) == 1)
+//    assert(Main.count(Set(1)) == 1)
+//  }
+//
+//  test("get file size") {
+//    val folder = System.getProperty("java.io.tmpdir")
+//    val file = new File(folder, "file1.txt")
+//    fillFileToSize(file, 5)
+//    println(file.getAbsolutePath)
+//    assert(Main.getFolderSize(file.toPath) == 5)
+//  }
+//
+//
+//  def fillFileToSize(file: File ,size: Int) {
+//    val stream = new FileOutputStream(file);
+//
+//    val buff = new Array[Byte](size)
+//    stream.write(buff)
+//    stream.flush()
+//    stream.close()
+//  }
+//
+//
+//}
+//
+//class ScalaByte{
+//  val bytes:Array[Byte] = "this is my test".getBytes()
+//}
